341530f956d3ee67510e210b5226268bcb58c537
qwet700 <trungtnd@unicloud.com.vn>
2023-11-16 09:38:47 +0700

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
-	r := mux.NewRouter().StrictSlash(true)
-	routes.ImpItem(r)
-	http.Handle("/", r)
-	fmt.Printf("Running on port 33060")
-	log.Fatal(http.ListenAndServe("localhost:33060", r))
+	r := chi.NewRouter().StrictSlash(true)
+	// routes.ImpItem(r)
+	// http.Handle("/", r)
+	// fmt.Printf("Running on port 33060")
+	// log.Fatal(http.ListenAndServe("localhost:33060", r))
+
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	http.ListenAndServe(":3000", r)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/qwet700/Booking-doctor
+
+go 1.21.3
+
+require github.com/gorilla/mux v1.8.1
